e9f1c041d6e170ac926e074f30a5bfce38fee290
Marta Stanczyk <marta.stanczyk97@gmail.com>
2019-11-19 18:07:13 +0100

    Initial commit

---
+++ src/main/java/student/was/Aplikacja/szkoleniowa/warsztaty/at/skni/WasApplication.java
@@ -0,0 +1,13 @@
+package student.was.Aplikacja.szkoleniowa.warsztaty.at.skni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/student/was/Aplikacja/szkoleniowa/warsztaty/at/skni/WasiApplicationTests.java
@@ -0,0 +1,13 @@
+package student.was.Aplikacja.szkoleniowa.warsztaty.at.skni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
